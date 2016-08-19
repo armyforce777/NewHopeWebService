9e40ecd416776b2ddd02e68d8ddb9cfbde3ec70e
Jackie <yujia.feng@LHP-LW7-7807PX1>
2016-08-19 16:29:12 -0400

    updates

---
+++ NewHopeWebService/Scripts/NewHopeApp/app.js
@@ -0,0 +1,12 @@
+﻿(function () {
+    //create a module
+    var app = angular.module('NewHopeApp',['ngRoute']);
+
+
+    //create a controller
+    app.controller('UserController', function ($scope) {
+        $scope.message = "This is New Hope Web Site!!!";
+    });
+    
+
+})();
